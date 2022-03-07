f5d11713d7edb3269155ad2765419b1328dad61f
lish1294 <elishaclerigo@gmail.com>
2022-03-07 10:33:27 -0800

    Update Loader.js

--- Loader.js
+++ Loader.js
@@ -1,82 +1,11 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
-//import CircularProgress from '@material-ui/core/CircularProgress';
 import ViewSDKClient from './ViewSDKClient.js';
-
-//under construction
-//import s3Proxy from 's3-proxy';
-//import express from 'express';
-//under construction
 
 
 const Loader = () => {
-    // const app = express();
-    // app.get('9112250_.pdf', s3Proxy({
-    //     bucket: 'aws-onecrew-test-public',//prefix: 'optional_s3_path_prefix',
-    //     accessKeyId: 'aws_access_key_id',
-    //     secretAccessKey: 'aws_secret_access_key',//overrideCacheControl: 'max-age=100000',
-    //     defaultKey: 'index.html'
-    // }));
-    // const underconstruction = () => {
-    //     //underconstruction
-    //     const express = require('express');
-    //     const request = require('request');
-    //     const app = express();
-    //     //
-    //     app.get('/6861226_uploading.pdf', (res) =>{
-    //         request(
-    //             { url: 'https://aws-onecrew-test-public.s3.us-west-1.amazonaws.com/6861226_uploading.pdf' },
-    //             (error, response, body) => {
-    //                 if (error || response.statusCode !== 200) {
-    //                   return res.status(500).json({ type: 'error', message: error.message });
-    //                 }
-              
-    //                 res.json(JSON.parse(body));
-    //             }
-    //         )
-    //     });
-    // }
 
     const underconstruction = () => {
-        //     var AWS = require("aws-sdk");
-            
-        //     //var s3 = new AWS.s3(options);
-        // fetch('https://aws-onecrew-test-public.s3.us-west-1.amazonaws.com/6861226_uploading.pdf', {method: 'GET'})
-        // .then(res => {
-        //     console.log(res);
-        //     return res.blob();
-        //   })
-        //   .then(blob => {
-        //     var url = window.URL.createObjectURL(blob);
-        //     var a = document.createElement('a');
-        //     a.href = url;
-        //     //a.download = 'whatisthis.pdf';
-        //     document.body.appendChild(a); 
-        //     a.click();  
-        //     setTimeout(
-        //       _ => { window.URL.revokeObjectURL(url); }, 
-        //       60000); 
-        //     a.remove(); 
-        //   })
-        //   .catch(err => {
-        //     console.error('err: ', err);
-        //   })
-        /*
-
-        var s3 = new AWS.S3({apiVersion: '2006-03-01', region: 'us-west-2'});
-        var params = {
-            Bucket: 'aws-onecrew-test-public',
-            Key: '6861226_uploading.pdf',
-            Body: 'Uploaded text using the promise-based method!'
-        };
-        var putObjectPromise = s3.putObject(params).promise();
-        putObjectPromise.then(function(data) {
-            console.log('Success');
-        }).catch(function(err) {
-            console.log(err);
-        });
-
-        */
     }
     underconstruction();
 
