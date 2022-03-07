66104642ef599c35921b35debc48105c03d54888
lish1294 <elishaclerigo@gmail.com>
2022-03-07 10:11:56 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import Uploader from "./components/Uploader"; //import CustomInput from "./components/CustomInput";
+import Loader from "./renderpdfinreach.js";
+
+function App() {
+  return (
+    <div className="App">
+      { <Uploader/> }
+
+      <br/> How Adobe PDF Embed works using their config:
+      { <Loader/> }
+      Around the web I might have seen the SDK work with AWS with previewFileUsingFilePromise().
+      <div>
+      <br/> EC's S3 Lambdas <br/>
+      Uploads to S3 instance: <a href="https://codepen.io/eclerigo/pen/vYWMWmb"> https://codepen.io/eclerigo/pen/vYWMWmb </a>  <br/>
+      Gets an arbitrary Presigned URL: <a href="https://codepen.io/eclerigo/pen/ExbJbNb"> https://codepen.io/eclerigo/pen/ExbJbNb </a>  <br/>
+      </div>
+    </div>
+
+
+  );
+}
+
+export default App;
